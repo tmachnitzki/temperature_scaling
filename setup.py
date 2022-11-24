48ccad8d023c640a759f66436ab54e845324fe38
Tobias Schanz <t.schanz@outlook.de>
2022-11-24 08:07:26 +0100

    Make package installable

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="temperature-scaling",
+    version="0.1.0",
+    description="A simple way to calibrate your neural network. The temperature_scaling.py module can be easily used "
+                "to calibrated any trained model.",
+    url="https://github.com/tmachnitzki/temperature_scaling",
+    author="",
+    author_email="",
+    license="",
+    packages=[],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+    ],
+)
